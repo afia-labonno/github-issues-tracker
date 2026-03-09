555305dd188ac88f647a77f2c29de33398928a76
afia-labonno <afia.cse01@gmail.com>
2026-03-09 22:14:16 +0600

    Login functionality added and redirect to dashboard

---
+++ script.js
@@ -0,0 +1,14 @@
+
+const login = () => {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "admin123") {
+        alert("Login Successfull");
+        window.location.href = "dashboard.html" ;
+    } else {
+        alert("Invalid Credential");
+    }
+
+}
+
